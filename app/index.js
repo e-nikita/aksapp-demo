07997f7c169bb23c4e55a6b31456daccf1795f50
e-nikita <nikitasinghtomar21@gmail.com>
2026-05-16 23:35:10 +0530

    test argocd

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from AKS DevOps Project 🚀");
+  res.send("Hello from ArgoCD GitOps 🚀");
 });
 
 app.get("/health", (req, res) => {
